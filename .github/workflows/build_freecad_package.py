295e75037c1380831b4dc4155a9e3b834be511dc
Max Schlecht <maxschlecht@web.de>
2025-06-03 02:09:43 +0200

    Add simple error handling to build_freecad_package.py, sync uv

--- .github/workflows/build_freecad_package.py
+++ .github/workflows/build_freecad_package.py
@@ -3,6 +3,7 @@
 from argparse import ArgumentParser
 from itertools import chain
 from pathlib import Path
+from sys import stderr
 from xml.etree import ElementTree as ET
 from zipfile import ZIP_DEFLATED, ZipFile
 
@@ -20,15 +21,24 @@ def build_freecad_package(
     path: Path = Path(),
     output_path: Path = Path(),
     extra_files: list[Path] = [],
-):
+) -> bool:
     package_metadata = ET.fromstring((path / PACKAGE_XML).read_text())
-    version = package_metadata.find("version", NAMESPACES).text
+    if (version := package_metadata.find("version", NAMESPACES)) is None or not version.text:
+        error(f"failed to find 'version' in '{PACKAGE_XML}'")
+        return False
 
-    workbench = package_metadata.find("content", NAMESPACES).find("workbench", NAMESPACES)
-    subdirectory = workbench.find("subdirectory", NAMESPACES).text
-    package_path = path / subdirectory
+    if (
+        (content := package_metadata.find("content", NAMESPACES)) is None
+        or (workbench := content.find("workbench", NAMESPACES)) is None
+        or (subdirectory := workbench.find("subdirectory", NAMESPACES)) is None
+        or not subdirectory.text
+    ):
+        error(f"failed to find 'content.workbench.subdirectory' in '{PACKAGE_XML}'")
+        return False
 
-    version_name = f"free2ki_v{version.replace('.', '-')}"
+    package_path = path / subdirectory.text
+
+    version_name = f"free2ki_v{version.text.replace('.', '-')}"
     zip_file_path = output_path / f"{version_name}.zip"
     with ZipFile(zip_file_path, mode="w", compression=ZIP_DEFLATED) as zip_file:
         paths = chain(
@@ -39,6 +49,14 @@ def build_freecad_package(
         )
         for filepath in paths:
             zip_file.write(filepath, f"{version_name}/{filepath.relative_to(path)}")
+
+    return True
+
+
+def error(*values: object, prefix: str = "error: "):
+    global _last_end
+    output = f"\033[91m{prefix}{' '.join(map(str, values))}\033[0m"
+    print(output, file=stderr)
 
 
 if __name__ == "__main__":
@@ -53,6 +71,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    build_freecad_package(
+    result = build_freecad_package(
         Path(args.source), Path(args.out), [Path(extra_file) for extra_file in args.extra_files]
     )
+    exit(int(not result))
